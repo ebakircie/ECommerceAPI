dee25fd59a007a7f55e00c377311337b872fe33d
Ersin Bakırcı <ebakircie@gmail.com>
2022-08-15 19:41:37 +0300

    ProductController tested

--- Infrastructure/ECommerceAPI.Persistence/Contexts/ECommerceAPIDbContext.cs
+++ Infrastructure/ECommerceAPI.Persistence/Contexts/ECommerceAPIDbContext.cs
@@ -35,6 +35,7 @@ namespace ECommerceAPI.Persistence.Contexts
                 {
                     EntityState.Added => item.Entity.CreatedDate = DateTime.UtcNow,
                     EntityState.Modified => item.Entity.UpdatedDate = DateTime.UtcNow,
+                    _ => DateTime.UtcNow  // bunu yazmasaydık, delete işleminde, switch yapısında added yada modified değilse, sonuncu şart'a giriyor. delete edilmiş bir objeyi etmeye çalıştığı için patladı.
                 };
             }
 
---
+++ Presentation/ECommerceAPI.API/Controllers/ProductsController.cs
@@ -0,0 +1,72 @@
+﻿using ECommerceAPI.Application.Repositories;
+using ECommerceAPI.Application.ViewModels.Products;
+using ECommerceAPI.Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace ECommerceAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        // TEST CONTROLLER
+        private readonly IProductWriteRepository _productWriteRepository;
+        private readonly IProductReadRepository _productReadRepository;
+        public ProductsController(IProductWriteRepository productWriteRepository, IProductReadRepository productReadRepository)
+        {
+            _productWriteRepository = productWriteRepository;
+            _productReadRepository = productReadRepository;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok( _productReadRepository.GetAll(false));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult>Get(string id)
+        {
+            return Ok(await _productReadRepository.GetByIdAsync(id, false));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(CreateProductVM model) 
+        {
+            //Product product = new Product();
+            //product.Name = model.Name;
+            //product.Price = model.Price;
+            //product.Stock = model.Stock;
+            //await _productWriteRepository.AddAsync(product);
+            await _productWriteRepository.AddAsync(new()
+            {
+                Name = model.Name,
+                Price = model.Price,
+                Stock = model.Stock,
+            });
+            await _productWriteRepository.SaveAsync();
+            
+            return StatusCode((int)HttpStatusCode.Created);
+        }
+        [HttpPut]
+        public async Task<IActionResult> Put(UpdateProductVM model)
+        {
+            Product product = await _productReadRepository.GetByIdAsync(model.Id);
+            product.Stock = model.Stock;
+            product.Name = model.Name;
+            product.Price = model.Price;
+            await _productWriteRepository.SaveAsync();
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete (string id)
+        {
+            await _productWriteRepository.RemoveAsync(id);
+            await _productWriteRepository.SaveAsync();
+            return Ok();
+        }
+
+    }
+}
